a5eb0e989b27464634f9f437df5d577669fc7a6f
Developper2310 <yufurkan.2310@gmail.com>
2025-10-29 14:30:22 +0300

    main update

---
+++ IM-AGES/Yardım.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace IM_AGES
+{
+    partial class Yardım
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            linkLabel4 = new LinkLabel();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(58, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(361, 31);
+            label1.TabIndex = 0;
+            label1.Text = "213301069 - Yusuf Furkan Umutlu ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(58, 179);
+            label2.Name = "label2";
+            label2.Size = new Size(365, 31);
+            label2.TabIndex = 1;
+            label2.Text = "193301100 - Mahmut Alperen Atar";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(58, 265);
+            label3.Name = "label3";
+            label3.Size = new Size(290, 31);
+            label3.TabIndex = 2;
+            label3.Text = "213301022 - İsmail Sarıteke";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(58, 343);
+            label4.Name = "label4";
+            label4.Size = new Size(309, 31);
+            label4.TabIndex = 3;
+            label4.Text = "213301090 - Mustafa Nahsan";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel1.Location = new Point(470, 105);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(336, 28);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "https://github.com/Developper2310 ";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.BackColor = Color.Transparent;
+            linkLabel2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel2.Location = new Point(470, 185);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(277, 28);
+            linkLabel2.TabIndex = 5;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "https://github.com/EmparrioR";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.BackColor = Color.Transparent;
+            linkLabel3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel3.Location = new Point(470, 270);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(216, 28);
+            linkLabel3.TabIndex = 6;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "https://github/sar1teke";
+            linkLabel3.LinkClicked += linkLabel3_LinkClicked;
+            // 
+            // linkLabel4
+            // 
+            linkLabel4.AutoSize = true;
+            linkLabel4.BackColor = Color.Transparent;
+            linkLabel4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel4.Location = new Point(470, 349);
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.Size = new Size(229, 28);
+            linkLabel4.TabIndex = 7;
+            linkLabel4.TabStop = true;
+            linkLabel4.Text = "https://github/mustafaf8";
+            linkLabel4.LinkClicked += linkLabel4_LinkClicked;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(58, 41);
+            label5.Name = "label5";
+            label5.Size = new Size(619, 28);
+            label5.TabIndex = 8;
+            label5.Text = "Proje için geliştirmelerimiz devam etmektedir. Geri bildirimleriniz için :";
+            // 
+            // Yardım
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.desktop_wallpaper_blur_blue_gradient_cool_background;
+            ClientSize = new Size(844, 479);
+            Controls.Add(label5);
+            Controls.Add(linkLabel4);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Yardım";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Yardım";
+            Load += Yardım_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
+        private LinkLabel linkLabel4;
+        private Label label5;
+    }
+}
